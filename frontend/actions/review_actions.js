72019f599dc6d27459b3ba16315b149820947c52
Jerry Lau <jerryzlau@gmail.com>
2017-09-26 09:52:48 -0700

    Set up the review actions but need to work on user profile first

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,68 @@
+import * as ReviewAPI from '../util/review_api_util';
+
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const RECEIVE_REVIEWS = 'RECEIVE_REVIEWS';
+export const REMOVE_REVIEW = 'REMOVE_REVIEW';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS';
+export const CLEAR_REVIEW_ERRORS = 'CLEAR_REVIEW_ERRORS';
+
+//actions handling
+export const receiveReview = review => ({
+  type: RECEIVE_REVIEW,
+  review
+});
+
+export const receiveReviews = reviews => ({
+  type: RECEIVE_REVIEWS,
+  reviews
+});
+
+export const removeReview = review => ({
+  type: REMOVE_REVIEW,
+  review
+});
+
+//error handling
+export const receiveReviewErrors = errors => ({
+  type: RECEIVE_REVIEW_ERRORS,
+  errors
+});
+
+export const clearReviewErrors = () => ({
+  type: CLEAR_REVIEW_ERRORS
+});
+
+//create a review
+export const createReview = review => dispatch => (
+  ReviewAPI.createReview(review)
+                .then(newReview => (dispatch(receiveReview(newReview))),
+                 err => (dispatch(receiveReviewErrors(err.responseJSON))))
+);
+
+//fetch a review for review show page
+export const requestReview = reviewId => dispatch => (
+  ReviewAPI.fetchReview(reviewId)
+                .then(review => (dispatch(receiveReview(review))),
+                 err => dispatch(receiveReviewErrors(err.responseJSON)))
+);
+
+// fetch all of current restaurant's reviews
+export const requestRestaurantReviews = ({userId, restaurantId}) => dispatch => (
+  ReviewAPI.fetchRestaurantReviews(userId, restaurantId)
+                .then(reviews => (dispatch(receiveReviews(reviews))),
+                 err => (dispatch(receiveReviewErrors(err.responseJSON))))
+);
+
+// cancel a review
+export const destroyReview = reviewId => dispatch => (
+  ReviewAPI.destroyReview(reviewId)
+                .then(review => (dispatch(removeReview(review))),
+                 err => dispatch(receiveReviewErrors(err.responseJSON)))
+);
+
+// modify a reservaiton
+export const editReview = review => dispatch => (
+  ReviewAPI.updateReview(review)
+                .then(newReview => (dispatch(receiveReview(newReview))),
+                 err => dispatch(receiveReviewErrors(err.responseJSON)))
+);
--- frontend/util/review_api_util.js
+++ frontend/util/review_api_util.js
@@ -1,3 +1,4 @@
+//create review
 export const createReview = review => (
   $.ajax({
     method: 'POST',
@@ -6,6 +7,7 @@ export const createReview = review => (
   })
 );
 
+//show review
 export const fetchReview = reviewId => (
   $.ajax({
     method: 'GET',
@@ -13,6 +15,7 @@ export const fetchReview = reviewId => (
   })
 );
 
+//get user profile reviews
 export const fetchUserReviews = (userId, restaurantId) => (
   $.ajax({
     method: 'GET',
@@ -21,10 +24,28 @@ export const fetchUserReviews = (userId, restaurantId) => (
   })
 );
 
+//get restaurant reviews
 export const fetchRestaurantReviews = (userId, restaurantId) => (
   $.ajax({
     method: 'GET',
     url: '/api/reviews',
     data: {userId, restaurantId}
+  })
+);
+
+//delete a review
+export const destroyReview = reviewId => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/reviews/${reviewId}`
+  })
+);
+
+//update a review 
+export const updateReview = review => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/reviews/${review.id}`,
+    data: {review}
   })
 );
